32255421d3624d09f4416ba531bf1683864840d8
Toroko <patthamon.chr@gmail.com>
2026-04-08 19:03:29 +0800

    run optimization function

--- aerodinamic_model.py
+++ aerodinamic_model.py
@@ -17,3 +17,24 @@ class BioFilterSim:
         delta_p = (self.air_viscosity * thickness * velocity) / permeability
         return delta_p
     
+    def run_optimization_study(self):
+        #Create dataset (1cm to 5cm)
+        #Permeability (k) based on fiber packing density
+        thicknesses = np.linspace(0.01,0.01,10)
+        velocities = [2.0,2.5,3.0]
+
+        #high permeability (loose fiber) vs low permeability (dense fiber)
+        permeability_range = [1.0e-9, 5.0e-9, 1.0e-8]
+
+        results = []
+        for v in velocities:
+            for k in permeability_range:
+                for t in thicknesses:
+                    dp = self.calculate_pressure_drop(v, t, k)
+                    results.append({
+                        'Velocity_ms':v,
+                        'Permeability_k':k,
+                        'Thickness_m':t,
+                        'PressureDrop_pa':dp,
+                        'Safe_Limit': dp < 50 # 50 Pa is a common safety threshold
+                    })
